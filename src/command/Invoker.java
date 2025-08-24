2b51c2cc5ab8fde83154573f9d9f742a67d56b6d
KevinU20221275 <U20221275@univo.edu.sv>
2025-08-23 19:39:34 -0600

    add Invoker

--- src/command/Invoker.java
+++ src/command/Invoker.java
@@ -1,4 +1,20 @@
 package command;
 
+/**
+ * Clase Invoker del patrón Command.
+ * Es responsable de recibir un comando y ejecutar la acción solicitada.
+ *
+ * Permite desacoplar el emisor de la acción del receptor,
+ * facilitando la ejecución de comandos de manera flexible.
+ */
 public class Invoker {
+    private Command command;
+
+    public void setCommand(Command command){
+        this.command = command;
+    }
+
+    public void executeReservation(){
+        command.execute();
+    }
 }
