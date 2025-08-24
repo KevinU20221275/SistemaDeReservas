88c6ca2a4899e242591898596ea5415fbcb4e463
KevinU20221275 <U20221275@univo.edu.sv>
2025-08-23 19:27:29 -0600

    add ReservationFactory interface

--- src/factory/ReservationFactory.java
+++ src/factory/ReservationFactory.java
@@ -1,4 +1,20 @@
 package factory;
 
+import reservations.Reservation;
+
+/**
+ * Interfaz que define el método fábrica para crear objetos de tipo {@link Reservation}.
+ *
+ * Implementa el patrón de diseño Factory Method, permitiendo delegar la instanciación
+ * de diferentes tipos de reservas (Hotel, Vuelo, Auto) a subclases específicas.
+ *
+ * De esta forma, el código cliente puede crear reservas de manera flexible
+ * sin acoplarse a clases concretas.
+ *
+ * @see factory.HotelReservationFactory
+ * @see factory.FlightReservationFactory
+ * @see factory.CarReservationFactory
+ */
 public interface ReservationFactory {
+    Reservation createReservation();
 }
