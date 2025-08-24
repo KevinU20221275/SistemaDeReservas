a6ff16a4704bce2f4fe95a611511e941f307553e
KevinU20221275 <U20221275@univo.edu.sv>
2025-08-23 19:31:51 -0600

    add SpaDecorator

--- src/decorator/SpaDecorator.java
+++ src/decorator/SpaDecorator.java
@@ -1,4 +1,43 @@
 package decorator;
 
-public class SpaDecorator {
+import reservations.Reservation;
+
+/**
+ * Decorador concreto que añade el servicio adicional de acceso al Spa
+ * a una reserva existente.
+ *
+ * Aplica un cargo extra al precio de la reserva y agrega la descripción del servicio como extra.
+ *
+ * Forma parte del patrón de diseño Decorator.
+ *
+ */
+public class SpaDecorator extends ReservationDecorator {
+    public SpaDecorator(Reservation baseReservation) {
+        super(baseReservation);
+    }
+
+    @Override
+    public void showDetails() {
+        super.showDetails();
+    }
+
+    /**
+     * Añade el cargo adicional del servicio Spa y agrega el extra correspondiente.
+     */
+    @Override
+    public void reserve() {
+        baseReservation.reserve();
+        baseReservation.extraServiceCharge(100F, "Acceso al Spa");
+        baseReservation.addExtraService("Acceso al Spa");;
+    }
+
+    @Override
+    public double getPrice() {
+        return super.getPrice();
+    }
+
+    @Override
+    public void setState(String state) {
+        baseReservation.setState(state);
+    }
 }
