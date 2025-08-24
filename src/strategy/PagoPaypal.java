509737e09f11bcdf89879891b91da26d7910976a
KevinU20221275 <U20221275@univo.edu.sv>
2025-08-23 19:34:36 -0600

    add PagoPaypal class

--- src/strategy/PagoPaypal.java
+++ src/strategy/PagoPaypal.java
@@ -1,4 +1,31 @@
 package strategy;
 
-public class PagoPaypal {
+
+/**
+ * Clase concreta que implementa la estrategia de pago mediante PayPal.
+ *
+ * Implementa la interfaz {@link PagoStrategy} para definir el comportamiento de pago.
+ */
+public class PagoPaypal implements PagoStrategy {
+    private String payPalAccount ;
+
+    /**
+     * Constructor que recibe la cuenta de PayPal.
+     *
+     * @param payPalAccount Dirección de correo asociada a la cuenta PayPal.
+     */
+    public PagoPaypal(String payPalAccount) {
+        this.payPalAccount = payPalAccount;
+    }
+
+    /**
+     * Método que realiza el pago utilizando PayPal.
+     *
+     * @param amount Monto total a pagar.
+     */
+    @Override
+    public void paid(Double amount) {
+        System.out.println("Pagando por payal: $" + amount);
+        System.out.println("Correo: " + payPalAccount);
+    }
 }
