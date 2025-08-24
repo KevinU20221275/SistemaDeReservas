334aedf7a21654ac226d95e562f55a1e8c790d40
KevinU20221275 <U20221275@univo.edu.sv>
2025-08-23 19:31:09 -0600

    add utility class GetFactoryReservation

--- src/utils/GetFactoryReservation.java
+++ src/utils/GetFactoryReservation.java
@@ -1,4 +1,30 @@
 package utils;
 
+import factory.CarReservationFactory;
+import factory.FlightReservationFactory;
+import factory.HotelReservationFactory;
+import factory.ReservationFactory;
+
+/**
+ * Clase utilitaria para obtener la factory de la reserva.
+ */
 public class GetFactoryReservation {
+    /**
+     * Obtiene la ReservationFactory de la reserva correspondiente según la opción seleccionada.
+     *
+     * @param option la opción de reserva seleccionada (1: Hotel, 2: Vuelo, 3: Auto)
+     * @return la factory de reserva seleccionada (ReservationFactory)
+     */
+    public static ReservationFactory getFactoryReservation(int option){
+        ReservationFactory factory = null;
+
+        switch (option) {
+            case 1 -> factory = new HotelReservationFactory();
+            case 2 -> factory = new FlightReservationFactory();
+            case 3 -> factory = new CarReservationFactory();
+            default -> System.out.println("Opción inválida.");
+        }
+
+        return factory;
+    }
 }
