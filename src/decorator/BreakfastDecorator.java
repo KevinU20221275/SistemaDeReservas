e1ac0ff9f3c53072605b2ac5f48244a81329c5b0
KevinU20221275 <U20221275@univo.edu.sv>
2025-08-23 19:33:20 -0600

    add BreakfastDecorator

--- src/decorator/BreakfastDecorator.java
+++ src/decorator/BreakfastDecorator.java
@@ -1,4 +1,41 @@
 package decorator;
 
-public class BreakfastDecorator {
+import reservations.Reservation;
+
+/**
+ * Decorador concreto que añade el servicio adicional de seguro a una reserva existente.
+ *
+ * Aplica un cargo extra al precio de la reserva y agrega la descripción del servicio como extra.
+ *
+ * Forma parte del patrón de diseño Decorator.
+ */
+public class BreakfastDecorator extends ReservationDecorator {
+    public BreakfastDecorator(Reservation baseReservation) {
+        super(baseReservation);
+    }
+
+    /**
+     * Añade el cargo adicional del Desayuno y agrega el extra correspondiente.
+     */
+    @Override
+    public void reserve() {
+        super.baseReservation.reserve();
+        super.baseReservation.extraServiceCharge(50F, "Desayuno Incluido"); // cargo por desayuno
+        super.baseReservation.addExtraService("Desayuno");
+    }
+
+    @Override
+    public void showDetails() {
+        super.showDetails();
+    }
+
+    @Override
+    public double getPrice() {
+        return super.getPrice();
+    }
+
+    @Override
+    public void setState(String estado) {
+        baseReservation.setState(estado);
+    }
 }
