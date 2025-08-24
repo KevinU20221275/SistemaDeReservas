374fac57f12bd227eb3b36b4cae1a8417be6906a
KevinU20221275 <U20221275@univo.edu.sv>
2025-08-23 19:37:03 -0600

    add ReservationBuilder

--- src/builder/ReservationBuilder.java
+++ src/builder/ReservationBuilder.java
@@ -1,4 +1,11 @@
 package builder;
 
+import reservations.Reservation;
+
 public interface ReservationBuilder {
+    ReservationBuilder setCliente(String clientName);
+    ReservationBuilder setDate(String date);
+    ReservationBuilder setDui(String dui);
+    Reservation build();
 }
+
