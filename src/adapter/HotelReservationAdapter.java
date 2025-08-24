4559748f7ab4cf4d5723c1a4957f87bf486110b7
KevinU20221275 <U20221275@univo.edu.sv>
2025-08-23 19:39:03 -0600

    add HotelReservationAdapter

--- src/adapter/HotelReservationAdapter.java
+++ src/adapter/HotelReservationAdapter.java
@@ -1,4 +1,33 @@
 package adapter;
 
-public class HotelReservationAdapter {
+import reservations.Reservation;
+import ui.Menu;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Adaptador para configurar reservas de Hotel.
+ */
+public class HotelReservationAdapter implements ReservationInputAdapter{
+    private String[] hotelList = {"El Marito, 4 Estrellas, $150", "El canopi, 4 Estrellas, $300", "El luxor, 5 Estrellas, $450"};
+
+    @Override
+    public void configureReservation(Scanner scanner, Reservation reservation) {
+        System.out.println("===== Hoteles Disponibles ===");
+        for (int i = 0; i < hotelList.length; i++) {
+            System.out.println(i+1 + ". " + hotelList[i]);
+        }
+
+        System.out.print("Seleccione su hotel: ");
+        int hotelOption = Menu.getValidatedOption(scanner, 1, hotelList.length); // válida la entrada del usuario
+
+        reservation.setPrice(hotelOption * 150);
+
+        if (reservation instanceof reservations.HotelReservation hotelReservation) {
+            int randomRoom = (int) (Math.random() * 200) + 1;
+            hotelReservation.setHotelName(this.hotelList[hotelOption - 1]);
+            hotelReservation.setRoom("HC" + randomRoom);
+        }
+    }
 }
