33015bf175daa1da74fe56197043891b0bb5cb38
KevinU20221275 <U20221275@univo.edu.sv>
2025-08-23 18:59:22 -0600

    add hotel reservation class

--- src/reservations/HotelReservation.java
+++ src/reservations/HotelReservation.java
@@ -1,11 +1,57 @@
-package factory;
+package reservations;
 
-import reservations.Reservation;
+/**
+ * Clase concreta que representa una reserva de hotel.
+ * Extiende de {@link ReservationAbstract} e incluye datos específicos como
+ * nombre del hotel y número de habitación.
+ */
+public class HotelReservation extends ReservationAbstract {
+    private String hotelName;
+    private String room;
 
-public class HotelReservation implements Reservation {
+    public HotelReservation(){
+        setType("Hotel");
+    }
 
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    /**
+     * Muestra los detalles completos de la reserva, incluyendo datos generales
+     * y datos específicos del hotel.
+     */
     @Override
     public void showDetails() {
+        super.showDetails();
+        System.out.println("Hotel: " + hotelName);
+        System.out.println("Habitacion: " + room);
+    }
 
+    @Override
+    public void reserve() {
+        super.reserve();
+    }
+
+    @Override
+    public void addExtraService(String extraService) {
+        super.addExtraSevice(extraService);
+    }
+
+    @Override
+    public double getPrice() {
+        return super.getPrice();
     }
 }
