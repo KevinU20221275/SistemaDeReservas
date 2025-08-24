94ffe13868cae053ef63c1f6250d93f4d851af11
KevinU20221275 <U20221275@univo.edu.sv>
2025-08-23 19:33:23 -0600

    add InsuranceDecorator

--- src/decorator/InsuranceDecorator.java
+++ src/decorator/InsuranceDecorator.java
@@ -1,4 +1,41 @@
 package decorator;
 
-public class InsuranceDecorator {
+import reservations.Reservation;
+
+/**
+ * Decorador concreto que añade el servicio adicional de seguro a una reserva existente.
+ *
+ * Aplica un cargo extra al precio de la reserva y agrega la descripción del servicio como extra.
+ *
+ * Forma parte del patrón de diseño Decorator.
+ */
+public class InsuranceDecorator extends ReservationDecorator {
+    public InsuranceDecorator(Reservation baseReservation){
+        super(baseReservation);
+    }
+
+    /**
+     * Añade el cargo adicional del seguro y agrega el extra correspondiente.
+     */
+    @Override
+    public void reserve(){
+        baseReservation.reserve();
+        baseReservation.extraServiceCharge(100F, "Seguro");
+        baseReservation.addExtraService("Seguro");
+    }
+
+    @Override
+    public void showDetails(){
+        super.showDetails();;
+    }
+
+    @Override
+    public double getPrice() {
+        return super.getPrice();
+    }
+
+    @Override
+    public void setState(String state) {
+        baseReservation.setState(state);
+    }
 }
