f19f93be77e698ace2fa3b67402f2fead26da3e3
KevinU20221275 <U20221275@univo.edu.sv>
2025-08-23 19:35:06 -0600

    add utility class PagoStrategy

--- src/utils/GetPagoStrategy.java
+++ src/utils/GetPagoStrategy.java
@@ -1,4 +1,39 @@
 package utils;
 
+import strategy.PagoPaypal;
+import strategy.PagoStrategy;
+import strategy.PagoTarjeta;
+
+import java.util.Scanner;
+
+/**
+ * Clase utilitaria para obtener la PagoStrategy segun la opcion.
+ */
 public class GetPagoStrategy {
+    /**
+     * Obtiene la estrategia de pago correspondiente según la opción seleccionada.
+     *
+     * @param paidOption la opción de método de pago seleccionada (1: Tarjeta, 2: Paypal)
+     * @param scanner el objeto Scanner para capturar datos adicionales si son necesarios
+     * @return la estrategia de pago seleccionada (PagoStrategy)
+     */
+    public static PagoStrategy getPagoStrategy(int paidOption, Scanner scanner){
+        PagoStrategy paymentMethod = null;
+
+        switch (paidOption){
+            case 1:
+                String cardNumber = ReservationFields.getValidCardNumber(scanner);
+                paymentMethod = new PagoTarjeta(cardNumber);
+                break;
+            case 2:
+                String payPalAccount = ReservationFields.getPaypalAccount(scanner);
+                paymentMethod = new PagoPaypal(payPalAccount);
+                break;
+            default:
+                System.out.println("Metodo de pago no soportado");
+                break;
+        }
+
+        return paymentMethod;
+    }
 }
