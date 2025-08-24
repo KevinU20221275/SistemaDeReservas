951d70984c3aa6bdd7def2246cfe4d27e0a9ded7
KevinU20221275 <U20221275@univo.edu.sv>
2025-08-23 19:39:38 -0600

    add ReservationCommand

--- src/command/ReservationCommand.java
+++ src/command/ReservationCommand.java
@@ -1,4 +1,33 @@
 package command;
 
-public class ReservationCommand {
+import facade.ReservationFacade;
+
+/**
+ * Clase concreta que implementa la interfaz Command.
+ * Encapsula la acción de realizar una reserva, delegando la operación
+ * a la clase ReservationFacade para simplificar la ejecución del proceso.
+ *
+ * Forma parte del patrón de diseño Command.
+ *
+ */
+public class ReservationCommand implements Command {
+    private ReservationFacade reservation;
+
+    /**
+     * Constructor de la clase.
+     *
+     * @param reservation Instancia de ReservationFacade que ejecutará la reserva.
+     */
+    public ReservationCommand(ReservationFacade reservation) {
+        this.reservation = reservation;
+    }
+
+    /**
+     * Ejecuta el comando, llamando al método makeReservation() del Facade.
+     */
+    @Override
+    public void execute() {
+        System.out.println("Ejecutando comando para reserva....");
+        this.reservation.makeReservation();
+    }
 }
