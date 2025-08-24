c114bcddb69e958bea679c773e80abae3f639774
KevinU20221275 <U20221275@univo.edu.sv>
2025-08-23 19:34:44 -0600

    add PagoTarjeta class

--- src/strategy/PagoTarjeta.java
+++ src/strategy/PagoTarjeta.java
@@ -1,4 +1,41 @@
 package strategy;
 
-public class PagoTarjeta {
+/**
+ * Estrategia concreta para realizar el pago mediante tarjeta de crédito.
+ * Implementa la interfaz {@link PagoStrategy}.
+ */
+public class PagoTarjeta implements PagoStrategy {
+    private String cardNumber;
+
+    /**
+     * Constructor que recibe el número de tarjeta.
+     *
+     * @param cardNumber Número de tarjeta de crédito (en formato String).
+     */
+    public PagoTarjeta(String cardNumber){
+        this.cardNumber = cardNumber;
+    }
+
+    /**
+     * Formatea el número de tarjeta ocultando los últimos 6 dígitos por razones de seguridad.
+     *
+     * @return Número de tarjeta formateado (ej: 1234567XXXXXX).
+     */
+    public String formatCardNumber(){
+        int lenght = cardNumber.length();
+
+        String formatNumber = cardNumber.substring(0, lenght - 6);
+        return formatNumber + "XXXXXX";
+    }
+
+    /**
+     * Realiza el pago mostrando el monto y el número de tarjeta formateado.
+     *
+     * @param amount Monto a pagar.
+     */
+    @Override
+    public void paid(Double amount) {
+        System.out.println("Pagando con targeta: $" + amount);
+        System.out.println("Targeta: " + formatCardNumber());
+    }
 }
