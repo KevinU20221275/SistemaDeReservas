d411e5e5126b004c9e890ee55540e0521c84fcf0
KevinU20221275 <U20221275@univo.edu.sv>
2025-08-23 19:38:37 -0600

    add ReservationInputAdapter interface

--- src/adapter/ReservationInputAdapter.java
+++ src/adapter/ReservationInputAdapter.java
@@ -1,4 +1,12 @@
 package adapter;
 
+import reservations.Reservation;
+
+import java.util.Scanner;
+
+/**
+ * Adapter para unificar la entrada de datos en la configuración de reservas.
+ */
 public interface ReservationInputAdapter {
+    void configureReservation(Scanner scanner, Reservation reservation);
 }
